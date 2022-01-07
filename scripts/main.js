aef4cdbca4c459c4c0ea93a1c2e585f85af42dce
starcanmd <serimhande@gmail.com>
2022-01-07 01:03:45 -0500

    Add button for a personalized welcome message

--- scripts/main.js
+++ scripts/main.js
@@ -1,10 +1,68 @@
 let myImage = document.querySelector('img');
 
 myImage.onclick = function() {
+
 	let mySrc = myImage.getAttribute ('src');
+
 	if(mySrc === 'images/firefox-icon.png') {
+
 		myImage.setAttribute('src','images/fox.gif');
+
 	}	else {
+
 		myImage.setAttribute('src','images/firefox-icon.png');
+
 	}
+}
+
+let myButton = document.querySelector('button');
+
+let myHeading = document.querySelector('h1');
+
+function setUserName() {
+
+	let myName = prompt('Please enter your name.');
+
+	if (!myName) {
+
+		noUserName();
+
+	}
+
+}
+
+function noUserName() {
+
+	let myName = prompt('You might have forgotten something :)');
+
+	if (!myName) {
+
+		noUserName();
+
+	} else {
+
+		localStorage.setItem ('name', myName);
+
+		myHeading.textContent = 'Mozilla is cool, ' + myName;
+	}
+
+}
+
+
+if(!localStorage.getItem('name')) {
+
+  setUserName();
+
+} else {
+
+  let storedName = localStorage.getItem('name');
+
+  myHeading.textContent = 'Mozilla is cool, ' + storedName;
+
+}
+
+myButton.onclick = function() {
+
+  setUserName();
+
 }
